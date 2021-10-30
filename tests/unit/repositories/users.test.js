0b4ab6449ee3dc5693bafae686c7a1878b6dbe99
Yuri Vyatkin <yurivyatkin@users.noreply.github.com>
2021-10-30 21:55:45 +1300

    Add a test for adding a user

---
+++ tests/unit/repositories/users.test.js
@@ -0,0 +1,34 @@
+const Chance = require('chance');
+
+const { usersRepository } = require('../../../src/repositories/inMemory');
+
+const {
+  User,
+  constants: {
+    userConstants: { genders },
+  },
+} = require('../../../src/entities');
+
+const chance = new Chance();
+
+describe('Users repository', () => {
+  test('New user should be added and returned', async () => {
+    const testUser = new User({
+      name: chance.name(),
+      lastName: chance.last(),
+      gender: genders.FEMALE,
+      meta: {
+        hair: { color: 'black' },
+      },
+    });
+    const addedUser = await usersRepository.add(testUser);
+    expect(addedUser).toBeDefined();
+    expect(addedUser.id).toBeDefined();
+    expect(addedUser.name).toBe(testUser.name);
+    expect(addedUser.lastName).toBe(testUser.lastName);
+    expect(addedUser.gender).toBe(testUser.gender);
+    expect(addedUser.meta).toBe(testUser.meta);
+  });
+  test('New user should be deleted', async () => {});
+  test('New user should be updated', async () => {});
+});
