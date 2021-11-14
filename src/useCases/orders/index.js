42cdc310034f12b8896c669b43ea7655718fd729
Yuri Vyatkin <yurivyatkin@users.noreply.github.com>
2021-11-14 21:28:40 +1300

    Add updateOrder use case

--- src/useCases/orders/index.js
+++ src/useCases/orders/index.js
@@ -1,7 +1,9 @@
 const addOrderUseCase = require('./addOrder.useCase');
 const getOrderByIdUseCase = require('./getOrderById.useCase');
+const updateOrderUseCase = require('./updateOrder.useCase');
 
 module.exports = {
   addOrderUseCase,
   getOrderByIdUseCase,
+  updateOrderUseCase,
 };
