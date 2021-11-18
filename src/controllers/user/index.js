578ce4fae333296ff2db2ee63e41e921572e8e4e
Yuri Vyatkin <yurivyatkin@users.noreply.github.com>
2021-11-18 23:05:45 +1300

    Add getUserById controller

--- src/controllers/user/index.js
+++ src/controllers/user/index.js
@@ -1,7 +1,9 @@
 const addUserController = require('./addUser.controller');
+const getUserByIdController = require('./getUserById.controller');
 
 module.exports = (dependencies) => {
   return {
     addUserController: addUserController(dependencies),
+    getUserByIdController: getUserByIdController(dependencies),
   };
 };
