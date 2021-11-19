7f347fbb2c2d155b43e81fcbc2be075cd6421a05
Yuri Vyatkin <yurivyatkin@users.noreply.github.com>
2021-11-19 23:01:28 +1300

    Add updateUser controller

--- src/controllers/user/index.js
+++ src/controllers/user/index.js
@@ -1,9 +1,11 @@
 const addUserController = require('./addUser.controller');
 const getUserByIdController = require('./getUserById.controller');
+const updateUserController = require('./updateUser.controller');
 
 module.exports = (dependencies) => {
   return {
     addUserController: addUserController(dependencies),
     getUserByIdController: getUserByIdController(dependencies),
+    updateUserController: updateUserController(dependencies),
   };
 };
