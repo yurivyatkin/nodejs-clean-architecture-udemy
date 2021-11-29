e75d3594cdbe03f5f47da6e563a21f7cb94967e3
Yuri Vyatkin <yurivyatkin@users.noreply.github.com>
2021-11-29 23:20:09 +1300

    Add updateProduct controller

--- src/controllers/product/index.js
+++ src/controllers/product/index.js
@@ -1,9 +1,11 @@
 const addProductController = require('./addProduct.controller');
 const getProductByIdController = require('./getProductById.controller');
+const updateProductController = require('./updateProduct.controller');
 
 module.exports = (dependencies) => {
   return {
     addProductController: addProductController(dependencies),
     getProductByIdController: getProductByIdController(dependencies),
+    updateProductController: updateProductController(dependencies),
   };
 };
