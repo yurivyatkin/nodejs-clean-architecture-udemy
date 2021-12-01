f849551eb4c5a6f4856da842492d540c55b640fa
Yuri Vyatkin <yurivyatkin@users.noreply.github.com>
2021-12-01 23:07:47 +1300

    Add product routes

--- src/frameworks/expressSpecific/routes/index.js
+++ src/frameworks/expressSpecific/routes/index.js
@@ -1,13 +1,16 @@
 const express = require('express');
 
 const usersRouter = require('./users');
+const productsRouter = require('./products');
 
 module.exports = (dependencies) => {
   const routes = express.Router();
 
   const users = usersRouter(dependencies);
+  const products = productsRouter(dependencies);
 
   routes.use('/users', users);
+  routes.use('/products', products);
 
   return routes;
 };
---
+++ src/frameworks/expressSpecific/routes/products.js
@@ -0,0 +1,23 @@
+const express = require('express');
+
+const { productControllers } = require('../../../controllers');
+
+module.exports = (dependencies) => {
+  const router = express.Router();
+
+  const {
+    addProductController,
+    getProductByIdController,
+    updateProductController,
+    deleteProductController,
+  } = productControllers(dependencies);
+
+  router
+    .route('/')
+    .post(addProductController)
+    .delete(deleteProductController)
+    .put(updateProductController);
+  router.route('/:id').get(getProductByIdController);
+
+  return router;
+};
